7822a3f63dd6818a0ad043685e88198d3039ebca
Aleksandr <aldrobakha@gmail.com>
2022-04-23 22:57:32 +0300

    add HW_SEM2_DOP2

--- HW_DOP2_S2/Program.cs
+++ HW_DOP2_S2/Program.cs
@@ -3,3 +3,17 @@
 // -4, 16 -> да
 // 25, 5 -> да
 // 8,9 -> нет
+
+Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(Math.Pow(numberA, 2) == numberB || Math.Pow(numberB, 2) == numberA)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}
