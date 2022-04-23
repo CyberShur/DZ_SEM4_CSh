53710ff8a21e2bc23bc223edfbdb46264e9d9329
Aleksandr <aldrobakha@gmail.com>
2022-04-23 23:42:50 +0300

    add HW2

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -2,3 +2,15 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.Write("Введите число: "); 
+string Input = Console.ReadLine();
+
+int sum = 0;
+
+for (int count = 0; count < Input.Length; count++)
+{
+    sum = sum + int.Parse(Input[count].ToString());
+}
+
+Console.WriteLine($"Сумма цифр числа {Input} равна {sum}");
