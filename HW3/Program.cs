8f505efaeb677390c54df86882e1666ef4a4c0b8
Aleksandr <aldrobakha@gmail.com>
2022-04-25 23:42:14 +0300

    add HW3

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -4,3 +4,36 @@
 // Поправка: на семинаре дал добро на заволнение массива рандомными числами. 
 // В оригинале нужно пользователю ввести строку - ряд чисел через запятую, а программа 
 // превращает эту строку в полноценный массив и выводит. Рекомендую вторым вариантом)
+
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите {i} элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write($"Вы ввели массив: "); 
+
+WriteArray(array);
+
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (i == 0)
+        {
+        Console.Write("[" + array[i] + ", ");
+        }
+        else if (array.Length - 1 - i == 0)
+        {
+            Console.Write(array[i] + "]");
+        }
+        else
+        {
+            Console.Write(array[i] + ", ");
+        }
+    }
+    Console.WriteLine();
+}
